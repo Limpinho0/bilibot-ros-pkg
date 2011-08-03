0dc51ca6c7de5d508a1a3b937b10b5949b838b0b
garrattgallagher <garrattgallagher@gmail.com>
2011-08-03 20:30:02 +0000

    VirtualSerial controlling motors with PWM

--- powerboard/LUFA/Demos/Device/ClassDriver/VirtualSerial/VirtualSerial.c
+++ powerboard/LUFA/Demos/Device/ClassDriver/VirtualSerial/VirtualSerial.c
@@ -73,11 +73,53 @@ void setupMotors(){
   
 }
 
+uint16_t handspeed;
+uint8_t handon;
+
+SIGNAL(TIMER3_COMPA_vect){
+  if(handon==0)
+    return;
+  l_HAND_SPD;
+  
+  
+}
+
+SIGNAL(TIMER3_COMPB_vect){
+  if(handon==0)
+    return;
+  
+  
+  
+}
+SIGNAL(TIMER3_OVF_vect){
+  if(handon==0)
+   l_HAND_SPD;
+  else{
+    
+    //set at the top:
+    h_HAND_SPD;
+  }  
+  
+}
+
+void increaseHandSpeed(){
+ if(handspeed<16000)
+   handspeed+=100;
+ OCR3A=handspeed;
+}
+
+void decreaseHandSpeed(){
+ if(handspeed>100)
+   handspeed-=100;
+ OCR3A=handspeed;
+}
+
+
   void setMotorMove(uint8_t c){
       
-      l_BASE_SPD;  //disable base motor
+ /*     l_BASE_SPD;  //disable base motor
       l_HAND_SPD;  //disable hand motor
-      
+ */     
       if(c=='Y'){
 	h_BASE_CTRL_B; 
 	l_BASE_CTRL_A;
@@ -88,25 +130,91 @@ void setupMotors(){
       }
       
       if(c=='T'){
+	if(handon)
+	  increaseHandSpeed();
+	else{
+	    handon=1;
+	    handspeed=1000;
+	    OCR3A=handspeed;
+	}
 	h_HAND_CTRL_B; 
 	l_HAND_CTRL_A;
+	return;
       }
       if(c=='G'){
+	if(handon){
+	  if(handspeed<16000)
+	    handspeed+=100;
+	    OCR3A=handspeed;
+	}
+	else{
+	    handon=1;
+	    handspeed=8000;
+	    OCR3A=handspeed;
+	}
 	l_HAND_CTRL_B; 
 	h_HAND_CTRL_A;
+	return;
       }
-      if(c == 'Y' || c=='H' || c=='T' || c=='G'){
-	h_BASE_SPD;  //enable base motor
-	h_HAND_SPD;  //enable hand motor
+      
+      if(c=='g'){
+	if(handon)
+	  decreaseHandSpeed();
+	else{
+	    handon=1;
+	    handspeed=8000;
+	    OCR3A=handspeed;
+	}
+	l_HAND_CTRL_B; 
+	h_HAND_CTRL_A;
+	return;
       }
-      else{
+//       if(c == 'Y' || c=='H' || c=='T' || c=='G'){
+// 	h_BASE_SPD;  //enable base motor
+// 	h_HAND_SPD;  //enable hand motor
+//       }
+      
+	handon=0;
 	l_BASE_CTRL_A;
 	l_BASE_CTRL_B;
 	l_HAND_CTRL_A;
 	l_HAND_CTRL_B;
-      }
+      
   }
 
+
+void setupHandOsc(){
+//TCCR3A
+//////////  10xxxxxx     //COM3A  OC3A clear on compare
+//  00xxxxxx     //COM3A  going to manually toggle pin
+//  xx0000xx     //COM1B/C  not toggling pins
+//  xxxxxx10      //fast PWM top= ICR3
+TCCR3A=0x82;
+  
+ //TCCR3B:
+//  0xxxxxxx     //input capture noise canelation off
+//  x1xxxxxx     //input capture edge select = rising
+//  xx0xxxxx     // N/C
+//  xxx11xxx      //waveform generation: fast pwm
+//  xxxxx001      //prescaler = 1
+//0x59
+TCCR3B=0x59;
+
+//lets set osc freq to 1Khz:
+ICR3=16000;
+//set both PWM to 50%:
+OCR3A=8000;
+OCR3B=8000;
+
+//TIMSK3   - timer 3 interrupts
+//  00xxxxxx     //N/C
+//  xx0xxxxx     //input capture interrupt  
+//  xxx0xxxx     // N/C
+//  xxxx011x      //output compare B,A on, C off
+//  xxxxxxx1      //timer overflow
+TIMSK3 = 0x07;
+  
+}
 
 void parseCommand(uint8_t c){
   if(c=='C'){
@@ -115,7 +223,7 @@ void parseCommand(uint8_t c){
     if(c=='K'){
       Toggle_KIN_EN;
   }
-  if(c == 'Y' || c=='H' || c=='T' || c=='G' || c=='s' || c=='S')
+  if(c == 'Y' || c=='H' || c=='T' || c=='G'|| c=='g' || c=='s' || c=='S')
     setMotorMove(c);
 }
 
@@ -175,6 +283,7 @@ void SetupHardware(void)
 SETUP_CREATE_PWR_EN;
 SETUP_KIN_EN;
   setupMotors();
+  setupHandOsc();
 	/* Disable watchdog if enabled by bootloader/fuses */
 //	MCUSR &= ~(1 << WDRF);
 //	wdt_disable();
