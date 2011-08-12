804d7bacf793cb9581ec163c2ffef159aaaaba82
garrattgallagher <garrattgallagher@gmail.com>
2011-08-12 03:48:45 +0000

    moving toward c++.  this version may not work, but it is less messed up than it will be soon

--- powerboard/LUFA/Demos/Device/ClassDriver/VirtualSerial/adc.h
+++ powerboard/LUFA/Demos/Device/ClassDriver/VirtualSerial/adc.h
@@ -6,23 +6,23 @@
 
 
 
-#define ADC_AD0_L adc_readings_l[0]
-#define ADC_AD1_L adc_readings_l[1]
-#define ADC_BATT_L adc_readings_l[2]
-#define ADC_GYRO_L adc_readings_l[3]
-#define ADC_BASE_CURR_L adc_readings_l[4]
-#define ADC_HAND_CURR_L adc_readings_l[5]
-#define ADC_TEMP_L adc_readings_l[6]
-#define ADC_BASE_POT_L adc_readings_l[7]
-
-#define ADC_AD0_H adc_readings_h[0]
-#define ADC_AD1_H adc_readings_h[1]
-#define ADC_BATT_H adc_readings_h[2]
-#define ADC_GYRO_H adc_readings_h[3]
-#define ADC_BASE_CURR_H adc_readings_h[4]
-#define ADC_HAND_CURR_H adc_readings_h[5]
-#define ADC_TEMP_H adc_readings_h[6]
-#define ADC_BASE_POT_H adc_readings_h[7]
+// #define ADC_AD0_L adc_readings_l[0]
+// #define ADC_AD1_L adc_readings_l[1]
+// #define ADC_BATT_L adc_readings_l[2]
+// #define ADC_GYRO_L adc_readings_l[3]
+// #define ADC_BASE_CURR_L adc_readings_l[4]
+// #define ADC_HAND_CURR_L adc_readings_l[5]
+// #define ADC_TEMP_L adc_readings_l[6]
+// #define ADC_BASE_POT_L adc_readings_l[7]
+// 
+// #define ADC_AD0_H adc_readings_h[0]
+// #define ADC_AD1_H adc_readings_h[1]
+// #define ADC_BATT_H adc_readings_h[2]
+// #define ADC_GYRO_H adc_readings_h[3]
+// #define ADC_BASE_CURR_H adc_readings_h[4]
+// #define ADC_HAND_CURR_H adc_readings_h[5]
+// #define ADC_TEMP_H adc_readings_h[6]
+// #define ADC_BASE_POT_H adc_readings_h[7]
 
 // #define ADC_AD0       (ADC_AD0_L + ADC_AD0_H<<8)
 // #define ADC_AD1       (ADC_AD1_L + ADC_AD1_H<<8)
@@ -44,9 +44,9 @@
 
 
 
-uint8_t adc_readings_l[8];
-uint8_t adc_readings_h[8];
-uint16_t adc_readings[8];
+//uint8_t adc_readings_l[8];
+//uint8_t adc_readings_h[8];
+uint8_t adc_readings[8];
 
 void setADCChannel(uint8_t channel){
     ADMUX &= 0xf8;
@@ -56,11 +56,11 @@ void setupADC(){
   //NOTE: make sure jtag interface is disabled in the fuses, or PF4-7 will be disabled.
   //ADMUX
   // 01xx xxxx    REFS: Volatge ref is AVCC
-  // xx0x xxxx    ADLAR: ADC result is right aligned
+  // xx1x xxxx    ADLAR: ADC result is right aligned
   // xxx0 0xxx    MUX 4:3 -> always zero because we are not doing differential readings
   // xxxx x000    MUX 2:0 -> arbitrarily set the channel to 0.  it can be changed by calling setADCChannel
   // 0x40
-  ADMUX=0x40;
+  ADMUX=0x60;
   
   //  ADCSRA
   //  1xxx xxxx   ADEN - Enable ADC
@@ -77,10 +77,10 @@ void setupADC(){
 SIGNAL(ADC_vect){ 
   uint8_t channel = ADMUX & 0x07;
   //make reading
-  adc_readings_l[channel] = ADCL;
-  adc_readings_h[channel] = ADCH;
-  adc_readings[channel] = adc_readings_h[channel];
-  adc_readings[channel]=adc_readings[channel]*0x0100+ adc_readings_l[channel];
+//  adc_readings_l[channel] = ADCL;
+  adc_readings[channel] = ADCH;
+//  adc_readings[channel] = adc_readings_h[channel];
+  //adc_readings[channel]=adc_readings[channel]*0x0100+ adc_readings_l[channel];
   
   
 //   stransmitf(" r: %i ",channel);
