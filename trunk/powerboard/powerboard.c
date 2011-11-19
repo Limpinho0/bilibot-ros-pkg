53f13982922c9925a2114e4ca4c954622298902f
drzaiusx11@gmail.com <drzaiusx11@gmail.com@bd973068-a9a2-62da-c42e-c2e58294a303>
2011-11-19 00:43:40 +0000

    cleanup comments

--- trunk/powerboard/powerboard.c
+++ trunk/powerboard/powerboard.c
@@ -1,46 +1,10 @@
-/*
-             LUFA Library
-     Copyright (C) Dean Camera, 2011.
 
-  dean [at] fourwalledcubicle [dot] com
-           www.lufa-lib.org
-*/
-
-/*
-  Copyright 2011  Dean Camera (dean [at] fourwalledcubicle [dot] com)
-
-  Permission to use, copy, modify, distribute, and sell this
-  software and its documentation for any purpose is hereby granted
-  without fee, provided that the above copyright notice appear in
-  all copies and that both that the copyright notice and this
-  permission notice and warranty disclaimer appear in supporting
-  documentation, and that the name of the author not be used in
-  advertising or publicity pertaining to distribution of the
-  software without specific, written prior permission.
-
-  The author disclaim all warranties with regard to this
-  software, including all implied warranties of merchantability
-  and fitness.  In no event shall the author be liable for any
-  special, indirect or consequential damages or any damages
-  whatsoever resulting from loss of use, data or profits, whether
-  in an action of contract, negligence or other tortious action,
-  arising out of or in connection with the use or performance of
-  this software.
-*/
-
-/** \file
- *
- *  Main source file for the VirtualSerial demo. This file contains the main tasks of
- *  the demo and is responsible for the initial application hardware configuration.
- */
-// extern "C" {
 #include "powerboard.h"
 #include "wireformat.h"
 #include "pinmapping.h"
 #include "adc.h"
 #include "simplehighmotor.h"
 #include "music.h"
-
 
 /** LUFA CDC Class driver interface configuration and state information. This structure is
  *  passed to all CDC Class driver functions, so that multiple instances of the same class
@@ -88,13 +52,6 @@ void transmitstring(char *tx, int len){
 	for(i=0;i<len;i++)
 		sendByte(tx[i]);
 }
-
-
-
-
-
-
-
 
 int MOTOR_DIR_MSK;
 
