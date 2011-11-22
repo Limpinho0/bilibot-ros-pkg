7ca6618525376576e75554f53214bef23bf29786
drzaiusx11@gmail.com <drzaiusx11@gmail.com@bd973068-a9a2-62da-c42e-c2e58294a303>
2011-11-22 02:58:46 +0000

    add ability for base to goto a given position

--- trunk/powerboard/simplehighmotor.h
+++ trunk/powerboard/simplehighmotor.h
@@ -3,15 +3,21 @@
 #include "lowlevelmotor.h"
 #include "adc.h"
 
-int16_t lastspeed;
+int16_t _lastSpeed;
+uint8_t _targetPosition;
 
 int MOTOR_DIR_MSK;
 #define MOTOR_DIR_UP 0x1
 #define MOTOR_DIR_DN 0x2
 
+#define MOTOR_DEFAULT_SPD 32767
+#define MOTOR_POS_ERR 5
+
 void setupMotors(){
     LowLevelSetup();
     startProfile();
+
+    _targetPosition = 0;
 
     MOTOR_DIR_MSK = MOTOR_DIR_UP | MOTOR_DIR_DN;
     
@@ -46,9 +52,33 @@ void HL_BaseSpeed(int16_t spd){
         (spd < 0 && ((MOTOR_DIR_MSK & MOTOR_DIR_DN) == MOTOR_DIR_DN)) ||
          spd == 0)
     {
-        lastspeed=spd;
+        _lastSpeed=spd;
         setBaseSpeed(spd);
     }
+}
+
+void HL_SetBasePosition(int8_t position)
+{
+    if (abs(ADC_BASE_POT-position) <= MOTOR_POS_ERR)  
+        HL_BaseSpeed(0);
+
+    if (ADC_BASE_POT > position)
+        HL_BaseSpeed(MOTOR_DEFAULT_SPD);
+    if (ADC_BASE_POT < position)
+        HL_BaseSpeed(-MOTOR_DEFAULT_SPD);
+
+    _targetPosition = position;
+}
+
+uint16_t HL_GetLastSpeed()
+{
+    return _lastSpeed;
+}
+
+void HL_CheckBasePosition()
+{
+    if (abs(ADC_BASE_POT-_targetPosition) <= MOTOR_POS_ERR)  
+        HL_BaseSpeed(0);
 }
 
 #endif
