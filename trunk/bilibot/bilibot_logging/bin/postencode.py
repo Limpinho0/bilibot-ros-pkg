e724e50f0edbd0d10fee084a4a309eeefca3002c
garrattgallagher@gmail.com <garrattgallagher@gmail.com@bd973068-a9a2-62da-c42e-c2e58294a303>
2011-12-13 23:22:35 +0000

    switching logs to installed version

--- trunk/bilibot/bilibot_logging/bin/postencode.py
+++ trunk/bilibot/bilibot_logging/bin/postencode.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('bilibot_logging')
-pkgpath=roslib.packages.get_pkg_dir('bilibot_logging')
+#import roslib; roslib.load_manifest('bilibot_logging')
+#pkgpath=roslib.packages.get_pkg_dir('bilibot_logging')
+pkgpath=##PATHHERE##
 import sys, urllib, base64
 input_filename = sys.argv[1]
 fin=open(pkgpath+"/identifier")
@@ -12,3 +13,5 @@ datawad = base64.encodestring(file("/tmp/toup", "rb").read())
 postwad = urllib.urlencode({"pass":passwad,"bot":botwad,"filedata":datawad, "filename":input_filename})
 file("/tmp/up.log", "wb").write(postwad)
 
+ls
+ls
--- trunk/bilibot/bilibot_logging/bin/runuploads.sh
+++ trunk/bilibot/bilibot_logging/bin/runuploads.sh
@@ -1,7 +1,8 @@
 cd $(rospack find bilibot_logging)
 svn up
+pkgpath=##PATHHERE##
 
 ident=$(grep -A20 "DEVNAME=/dev/sda\$" /var/log/udev | grep ID_SERIAL_SHORT | awk -F\= '{print $2}')
-test -e $(rospack find bilibot_logging)/identifier || echo $ident >  $(rospack find bilibot_logging)/identifier
+test -e $pkgpath/identifier || echo $ident >  $pkgpath/identifier
 
-$(rospack find bilibot_logging)/bin/uploadall
+$pkgpath/uploadall
